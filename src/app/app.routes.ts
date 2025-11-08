4ce56feedaf9477ee41cb141f01a6bdab5eb32b7
Maysel <y.camarasa@gmail.com>
2025-11-08 09:01:52 +0100

    Little fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './pages/game-page/game-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { FinalPageComponent } from './pages/final-page/final-page/final-page.component';
+import { FinalPageComponent } from './pages/final-page/final-page.component';
 
 export const routes: Routes = [
   {
--- src/app/pages/final-page/final-page.component.ts
+++ src/app/pages/final-page/final-page.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CvService } from '../../../services/cv.service';
-import { ConfettiComponent } from '../../../components/confetti/confetti.component';
-import { IconComponent } from '../../../icons/icon.component';
+import { CvService } from '../../services/cv.service';
+import { ConfettiComponent } from '../../components/confetti/confetti.component';
+import { IconComponent } from '../../icons/icon.component';
 import {
   checkIcon,
   cvIcon,
   downloadIcon,
   githubIcon,
-} from '../../../icons/icons.icon';
+} from '../../icons/icons.icon';
 
 @Component({
   selector: 'app-final-page',
